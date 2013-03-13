cd7195f994e0f231007082c928b27e18a4fcb767
Vid Luther <vid@zippykid.com>
2013-03-13 01:26:04 -0500

    starting to use the settings aPI. @todo proper validation

--- page.php
+++ page.php
@@ -25,14 +25,15 @@ class MixPanel {
   function insert_event()
   {
     $event_label = self::get_post_event_label(); 
+    $settings = (array) get_option( 'mixpanel_settings' );
 
-    if(!defined('MIXPANEL_TOKEN')) {
-      self::no_mixpanel_token_found(); 
-      return false; 
-    } 
+    if(!isset($settings['token_id'])) {
+      self::no_mixpanel_token_found();
+      return false;  
+    }
     echo "<script type='text/javascript'>
-	  mixpanel.register_once({ 'first_referrer': document.referrer });
-      mixpanel.track(\"$event_label\", {'referrer': document.referrer });
+	     mixpanel.register_once({ 'first_referrer': document.referrer });
+       mixpanel.track(\"WP $event_label\", {'referrer': document.referrer });
     </script> "; 
 
 
@@ -43,16 +44,19 @@ class MixPanel {
 
   /**
    * Adds the Javascript necessary to start tracking via MixPanel. 
+   * this gets added to the <head> section usually. 
+   *
    * @return [type] [description]
    */
   function insert_tracker()
   {
+    $settings = (array) get_option( 'mixpanel_settings' );
+    if(!isset($settings['token_id'])) {
+      self::no_mixpanel_token_found();
+      return false;  
+    }
 
-    if(!defined('MIXPANEL_TOKEN')) {
-      self::no_mixpanel_token_found(); 
-      return false; 
-    }  
-          require_once dirname(__FILE__) . '/mixpaneljs.php';  
+    require_once dirname(__FILE__) . '/mixpaneljs.php';  
   
 
     return true;  
